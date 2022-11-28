980e0d76afd29b0c3c164b7a5a16ce4f4e5855c1
Emīls Daivids Kļaviņš <41874384+jeffthechristian@users.noreply.github.com>
2022-11-28 22:43:02 +0200

    Movement (first person)

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    float xRotation = 0f;
+    public float sensitivity = 500;
+    public Transform playerBody;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, mouseX, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
